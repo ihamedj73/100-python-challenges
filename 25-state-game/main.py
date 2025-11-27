780366abdda638e86c0115a11b9efd0baf6db95a
Hamed Jahangiry <jahangiryh73@gmail.com>
2025-11-27 16:30:59 +0330

    Day 25

---
+++ 25-state-game/main.py
@@ -0,0 +1,38 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
+
+data = pd.read_csv("50_states.csv")
+state_len = len(data)
+
+guessed_states = []
+
+while len(guessed_states) < state_len:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/{state_len}  Guess the State",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        all_states = data.state.to_list()
+        states_to_learn = []
+        for state in all_states:
+            if state not in guessed_states:
+                states_to_learn.append(state)
+
+        to_learn_df = pd.Series(states_to_learn)
+        to_learn_df.to_csv("states_to_learn.csv")
+        break
+    get_state = data[data['state'] == answer_state]
+    if not get_state.empty:
+        guessed_states.append(answer_state)
+        found_state = get_state.iloc[0]
+        writer.goto(x=found_state['x'], y=found_state['y'])
+        writer.write(answer_state, align="center")
+
+
